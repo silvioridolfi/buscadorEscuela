8226b4ab5565b8c23f1c9e0abd091190867f1fa4
Silvio Ridolfi <61565667+silvioridolfi@users.noreply.github.com>
2025-02-14 22:28:16 -0300

    version
    version nueva

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,11 @@ const encodeSans = Encode_Sans({ subsets: ["latin"] })
 
 export default function Home() {
   return (
-    <main className={`min-h-screen bg-[#00AEC3] text-white p-4 ${encodeSans.className}`}>
-      <h1 className="mb-4 text-3xl font-bold">Buscador de establecimientos educativos</h1>
-
-      <SchoolSearch />
+    <main className={`min-h-screen bg-[#00AEC3] text-white ${encodeSans.className}`}>
+      <div className="p-4">
+        <h1 className="mb-4 text-3xl font-bold">Buscador de establecimientos educativos</h1>
+        <SchoolSearch />
+      </div>
     </main>
   )
 }
